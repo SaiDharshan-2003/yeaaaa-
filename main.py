3d6985733b7e33b589e264a31767dc98169c370c
SaiDharshan-2003 <75945823+SaiDharshan-2003@users.noreply.github.com>
2022-01-19 18:36:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 floor = 4
 initial = 0
-destination = int(input())
-if destination <= floor:
-    if destination == initial:
-        print('same floor')
-    elif destination > initial:
-        while initial < destination:
-            initial += 1
-        print(initial)
-    elif destination < initial:
-        while initial > destination:
-            initial -= 1
-        print(initial)
+x=1
+while x<=2:
+    destination = int(input("enter destination floor"))
+    if destination <= floor:
+        if destination == initial:
+            print('same floor')
+        elif destination > initial:
+            while initial <= destination:
+                initial += 1
+            print(initial-1)
+            initial-=1
+        elif destination < initial:
+            while initial >= destination:
+                initial -= 1
+            print(initial+1)
+            initial+=1
+        else:
+            pass
+
+
     else:
-        pass
-
-
-else:
-    print("invalid floor")
+        print("invalid floor")
